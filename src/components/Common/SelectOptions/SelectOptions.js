2dbbd61ad8dcd2015b586f5d80f7fa02d947caa8
Kazi Solayman <kazisolayman123@gmail.com>
2024-10-23 16:00:03 +0600

    fixed a little bug for the selector of pet booking

--- src/components/Common/SelectOptions/SelectOptions.js
+++ src/components/Common/SelectOptions/SelectOptions.js
@@ -9,9 +9,9 @@ const SelectOptions = ({options}) => {
         setSelected(false);
     }
     return (
-        <ul>
+        <ul className="relative">
             <li onClick={() => setSelected(!selected)} className="cursor-pointer text-gray-800 font-light border border-gray-300 p-2  rounded outline-[#5d3855a3] flex justify-between items-center">{selectedOption} {selected ? <HiChevronUp /> : <HiChevronDown />}</li>
-            <div className={`${selected || 'hidden'} bg-white p-2 rounded shadow-md `}>
+            <div className={`${selected || 'hidden'} bg-white p-2 rounded shadow-md absolute top-full left-0 w-full `}>
                 {options.map((option, index) => <li key={index} onClick={() => handleSelectOption(option)} className={`py-2 cursor-pointer px-2 hover:bg-gray-100 duration-100 rounded`}>{option}</li>)}
             </div>
         </ul>
