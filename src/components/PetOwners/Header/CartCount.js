c2475bd3846dcfd824a13201662d4a0b63cc9594
Kazi Solayman <kazisolayman123@gmail.com>
2024-11-16 18:13:55 +0600

    cart button with navigating to cart page

--- src/components/PetOwners/Header/CartCount.js
+++ src/components/PetOwners/Header/CartCount.js
@@ -1,9 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { HiOutlineShoppingBag, HiOutlineShoppingCart } from "react-icons/hi2";
 
 const CartCount = () => {
-    let cartItems = 10;
+    let cartItems = 3;
+    const router = useRouter()
     return (
-        <button className="text-primary relative"><HiOutlineShoppingBag className="text-xl" />
+        <button type="button" onClick={() => router.push("/cart")} className="text-primary relative"><HiOutlineShoppingBag className="text-xl" />
             <p className="text-xs text-white bg-primary rounded-full text-center leading-5 w-5 h-5 absolute -top-3 -right-3">{cartItems}</p>
         </button>
     );
