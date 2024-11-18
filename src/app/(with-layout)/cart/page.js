7bf7ee81340244b2b3dad80f37aee53269cb2369
Kazi Solayman <kazisolayman123@gmail.com>
2024-11-18 15:31:22 +0600

    cart page repsonsive

--- src/app/(with-layout)/cart/page.js
+++ src/app/(with-layout)/cart/page.js
@@ -2,7 +2,7 @@ import CartPage from '@/components/PetOwners/CartPage/CartPage';
 
 const Cart = () => {
     return (
-        <div className='container py-14 '>
+        <div className='container mx-auto max-md:px-3 xl:px-20 py-14 '>
             <h2 className='text-primary mb-7 font-semibold text-2xl'>Your Shopping Cart</h2>
             <div className=''>
                 <CartPage />
