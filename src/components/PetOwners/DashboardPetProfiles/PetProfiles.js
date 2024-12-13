9074e0508c406cf6448a665f88dbd2eb570235bf
Kazi Solayman <kazisolayman123@gmail.com>
2024-12-13 15:45:37 +0600

    modified pet profiles

--- src/components/PetOwners/DashboardPetProfiles/PetProfiles.js
+++ src/components/PetOwners/DashboardPetProfiles/PetProfiles.js
@@ -6,7 +6,7 @@ const PetProfiles = () => {
     const profiles = [
         {
             _id: "1",
-            image: "https://via.placeholder.com/150",
+            image: "/images/cat-cute.jpg",
             type: "Cat",
             name: "Buddy",
             age: "3 years",
@@ -40,8 +40,8 @@ const PetProfiles = () => {
         },
         {
             _id: "1",
-            image: "https://via.placeholder.com/150",
-            type: "Cat",
+            image: "/images/cute-dog.jpg",
+            type: "Dog",
             name: "Buddy",
             age: "3 years",
             breed: "Golden Retriever",
@@ -75,18 +75,18 @@ const PetProfiles = () => {
     ]
     return (
         <div className='grid grid-cols-3 gap-10 '>
-            {profiles.map(({ _id, type, name, age, breed, gender, weight, createdAt, updatedAt }) => (
+            {profiles.map(({ _id, type, name, image, age, breed, gender, weight, createdAt, updatedAt }) => (
                 <div key={_id} className='border rounded-md bg-white '>
-                    <Image src="/images/vet.png" alt="pet's-img" width={200} height={200} className='object-cover w-full h-56 rounded-t-md rounded-b-3xl' />
+                    <Image src={image} alt="pet's-img" width={200} height={200} className='object-cover w-full h-56 rounded-t-md rounded-b-3xl' />
                     <div className='p-3 mt-3'>
                         <ul className='text-gray-600 flex flex-col gap-3 mb-7'>
                             <li className='flex items-center justify-between text-sm'><span>Name</span>{name}</li>
                             <li className='flex items-center justify-between text-sm'><span>Age</span>{age}</li>
                             <li className='flex items-center justify-between text-sm'><span>Breed</span>{breed}</li>
                             <li className='flex items-center justify-between text-sm'><span>Gender</span>{gender}</li>
-                            {/* <li className='flex items-center justify-between text-sm'><span>Weight</span>{weight}</li>
+                            <li className='flex items-center justify-between text-sm'><span>Weight</span>{weight}</li>
                             <li className='flex items-center justify-between text-sm'><span>CreatedAt</span>{createdAt}</li>
-                            <li className='flex items-center justify-between text-sm'><span>UpdatedAt</span>{updatedAt}</li> */}
+                            <li className='flex items-center justify-between text-sm'><span>UpdatedAt</span>{updatedAt}</li>
                         </ul>
                         <Button variant={"primary"} classNames={"!w-full !text-base !font-normal"}>Update Profile</Button>
                     </div>
