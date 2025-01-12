89d95501e6caa1a9fa43da56005b617a59703eb7
Kazi Solayman <kazisolayman123@gmail.com>
2025-01-12 22:34:28 +0600

    shows 2 vet specialities in mobile version

--- src/pages/PetOwners/Appointments/Vets.js
+++ src/pages/PetOwners/Appointments/Vets.js
@@ -15,13 +15,14 @@ const Vets = ({ vets, currentPath }) => {
                             vet: `${name.toLowerCase()}`
                         }
                     }} key={_id} className='border rounded-md hover:shadow-md duration-150 p-3 grid sm:grid-cols-[5fr_3fr_2fr] gap-5 items-center'>
-                        <div className='flex sm:items-start gap-5'>
+                        <div className='flex sm:items-start gap-5 max-sm:gap-3'>
                             <div><Image src="/images/vet.png" alt="" width={100} height={100} className='rounded max-w-32' /></div>
                             <div>
                                 <h4 className='font-bold'>{name}</h4>
                                 <p className='text-sm mb-2'>{title}</p>
                                 <p className='text-sm text-gray-700 mb-1'>Specialities</p>
-                                <div className='flex gap-1 flex-wrap'>{specialities.map((speciality, index) => <p key={index} className='text-xs bg-primary p-1 text-white rounded'>{speciality}</p>)}</div>
+                                <div className='flex gap-1 flex-wrap sm:hidden'>{specialities.slice(0, 2).map((speciality, index) => <p key={index} className='text-xs bg-primary p-1 text-white rounded'>{speciality}</p>)}</div>
+                                <div className='flex gap-1 flex-wrap max-sm:hidden'>{specialities.map((speciality, index) => <p key={index} className='text-xs bg-primary p-1 text-white rounded'>{speciality}</p>)}</div>
                             </div>
                         </div>
                         <div className='max-sm:flex gap-5'>
