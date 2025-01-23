d3496cedf596d917036cb4fe2bf1c81aa5709276
Kazi Solayman <kazisolayman123@gmail.com>
2025-01-24 00:46:31 +0600

    user login works

--- src/components/Common/SignIn/SignIn.js
+++ src/components/Common/SignIn/SignIn.js
@@ -1,20 +1,58 @@
+"use client";
 import { FaFacebookF, FaGoogle } from "react-icons/fa6";
 import Link from "next/link";
 import Input from "../Form/Input";
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setAuthUser } from "@/redux/features/userSlice";
+import { useRouter } from "next/navigation";
 
 const SignInPage = () => {
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    })
+    const handleUserLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`http://localhost:8000/api/user/login`, user, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                withCredentials: true
+            })
+            if (response.status === 200) {
+                dispatch(setAuthUser(response?.data?.userDetails));
+                alert("User loggin successfull!");
+                router.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <div className="max-w-96 text-center flex justify-center mx-auto my-20 p-3">
             <div className="w-full">
                 <>
                     <h2 className="text-3xl font-semibold mb-10">Welcome back</h2>
-                    <form action="#" className="flex flex-col gap-3">
-                        <Input type="email" placeholder="Email address" />
-                        <Input type="password" placeholder="Password" />
+                    <form onSubmit={handleUserLogin} action="#" className="flex flex-col gap-3">
+                        <Input type="email" placeholder="Email address"
+                            name="email"
+                            onChange={(e) => setUser({ ...user, email: e.target.value })}
+                            value={user.email}
+                        />
+                        <Input type="password" placeholder="Password"
+                            name="password"
+                            onChange={(e) => setUser({ ...user, password: e.target.value })}
+                            value={user.password}
+                        />
                         <div className="flex">
-                        <Link className="text-primary" href="">Forgot password?</Link>
+                            <Link className="text-primary" href="">Forgot password?</Link>
                         </div>
-                        <input type="submit" value="Continue" className="bg-primary p-3 rounded-md cursor-pointer text-white mt-5" />
+                        <button type="submit" className="bg-primary p-3 rounded-md cursor-pointer text-white mt-5">Sign In </button>
                     </form>
                     <div className="my-5">
                         <p>Dont have an account? <Link href="/signup" className="text-primary">Sign Up</Link></p>
