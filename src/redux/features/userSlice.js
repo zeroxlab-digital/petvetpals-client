079507afe2e9ce59f7fcd5a437a52048c9b20a48
Kazi Solayman <kazisolayman123@gmail.com>
2025-01-27 23:01:45 +0600

    removed appointment state from user slice

--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -2,18 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
     name: "authUser",
-    initialState: { authUser: null, appointments: [] },
+    initialState: { authUser: null },
     reducers: {
         setAuthUser: (state, action) => {
             state.authUser = action.payload;
-        },
-        setAppointments: (state, action) => {
-            console.log("Current state:", state.appointments)
-            console.log("Payload recieved:", action.payload)
-            state.appointments.push(action.payload);
         }
     }
 })
-export const { setAuthUser, setAppointments } = userSlice.actions;
+export const { setAuthUser } = userSlice.actions;
 
 export default userSlice.reducer;
