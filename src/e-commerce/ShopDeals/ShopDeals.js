2cf98e955d89a876724640bd7163c7ae0f0360c5
Kazi Solayman <kazisolayman123@gmail.com>
2025-02-13 15:38:14 +0600

    modified the shop page

--- src/e-commerce/ShopDeals/ShopDeals.js
+++ src/e-commerce/ShopDeals/ShopDeals.js
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import Link from 'next/link';
 import { LuCat, LuDog, LuPill } from "react-icons/lu";
@@ -8,17 +9,32 @@ const ShopDeals = () => {
     const categories = useCategories();
     const pathname = usePathname();
     return (
-        <div className='grid grid-cols-2 sm:grid-cols-1 items-center gap-8 text-center'>
+        <div className='grid grid-cols-2 max-sm:grid-cols-1 items-center gap-8 text-center'>
             {categories.map((category) => <Link href={{
                 pathname: `${pathname.slice(1, pathname.length)}/${category.category_slug}`,
                 query: {
                     title: `${category.category_name.toLowerCase()}`
                 }
-            }} key={category.category_slug} className='border rounded-md hover:shadow-lg duration-200'>
-                <div className='bg-gray-300 bg-opacity-10 flex items-center justify-center py-7'>
-                    <LuDog className='text-5xl text-gray-800' />
-                </div>
-                <h3 className='py-5 font-semibold text-gray-900'>{category.category_name}</h3>
+            }} key={category.category_slug} className='border rounded-lg hover:shadow-lg duration-200 w-full h-32 text-center flex items-center justify-center relative group overflow-hidden '
+                style={{
+                    backgroundImage: `url('/images/cute-dog.jpg')`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                }}>
+                {/* Hover effect applied to background */}
+                <div
+                    className='absolute inset-0 transition-transform duration-500 ease-in-out group-hover:scale-110 z-[0]'
+                    style={{
+                        backgroundImage: `url('/images/cute-dog.jpg')`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat'
+                    }}
+                ></div>
+                {/* Dark overlay */}
+                <div className="absolute inset-0 bg-black opacity-30 z-[1]"></div>
+                <button className='font-bold text-white uppercase text-sm z-10 bg-transparent border group-hover:border-none group-hover:bg-primary duration-300 ease-in-out w-44 h-12 rounded-full '>{category.category_name}</button>
             </Link>
             )}
         </div>
