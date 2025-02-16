c2a70ea48c3647310109e13f1ac255cf19d67efe
Kazi Solayman <kazisolayman123@gmail.com>
2025-02-16 16:24:52 +0600

    added breadcrumb for e-commerce

---
+++ src/components/Common/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,37 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Breadcrumb = () => {
+    const pathname = usePathname();
+    const pathnameSlugs = pathname.split("/").filter(slug => slug);
+    return (
+        <nav className="text-gray-600 text-sm font-medium mb-5">
+            <ul className="flex uppercase">
+                <li>
+                    <Link href="/" className="hover:text-blue-500 duration-100">home</Link>
+                </li>
+                {pathnameSlugs.map((slug, index) => {
+                    const routeTo = "/" + pathnameSlugs.slice(0, index + 1).join("/");
+                    const isLast = index === pathnameSlugs.length - 1;
+
+                    return (
+                        <li key={routeTo} className="flex items-center">
+                            <span className="mx-2">/</span>
+                            {isLast ? (
+                                <span className="text-gray-700">{decodeURIComponent(slug)}</span>
+                            ) : (
+                                <Link href={routeTo} className="hover:text-blue-500 duration-100">
+                                    {decodeURIComponent(slug)}
+                                </Link>
+                            )}
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
+};
+
+export default Breadcrumb;
