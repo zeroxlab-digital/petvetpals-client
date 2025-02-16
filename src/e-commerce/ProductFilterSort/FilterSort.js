cb9ff783436fcf004e77fea5e4457d29fba967a0
Kazi Solayman <kazisolayman123@gmail.com>
2025-02-16 16:36:52 +0600

    added breadcrumb in other pages

--- src/e-commerce/ProductFilterSort/FilterSort.js
+++ src/e-commerce/ProductFilterSort/FilterSort.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from 'react';
 import { HiBarsArrowUp, HiMiniAdjustmentsHorizontal, HiOutlineBarsArrowDown, HiOutlineXCircle } from 'react-icons/hi2';
 
--- src/e-commerce/Products/Products.js
+++ src/e-commerce/Products/Products.js
@@ -1,4 +1,5 @@
 "use client";
+import Breadcrumb from '@/components/Common/Breadcrumb/Breadcrumb';
 import { setCart } from '@/redux/features/cartSlice';
 import { Rating } from '@mui/material';
 import Image from 'next/image';
@@ -7,6 +8,7 @@ import { usePathname } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { HiOutlineHeart, HiShoppingCart } from 'react-icons/hi2';
 import { useDispatch } from 'react-redux';
+import FilterSort from '../ProductFilterSort/FilterSort';
 
 const Products = ({ slug }) => {
     const pathname = usePathname();
@@ -42,45 +44,51 @@ const Products = ({ slug }) => {
         disptach(setCart({ product, order_quantity: 1 }));
     }
     return (
-        <div className='grid grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-5'>
-            {
-                products.length > 0 ?
-                    products.map(product => <div key={product._id} className='border pb-3 rounded-md'>
-                        <Link
-                            href={{
-                                pathname: `${href + '/' + product._id}`,
-                                query: {
-                                    title: `${product.name.toLowerCase()}`,
-                                    description: `${product.product_description.toLowerCase()}`
-                                }
-                            }}
-                        >
-                            <div className="bg-gray-300 bg-opacity-10 rounded w-auto h-52 mb-3">
-                                <Image src="/images/med1.webp" alt="product-img" width={100} height={100} className="w-full h-full object-cover rounded" />
-                            </div>
-                            <div className='px-3'>
-                                <h2 className="text-gray-900 hover:underline cursor-pointer mb-2">{product.name}</h2>
-                                <div className="flex items-center justify-between">
-                                    <h4 className="font-semibold text-gray-800">${product.price}</h4>
-                                    <div className="flex gap-1 items-center">
-                                        <Rating name="half-rating-read" size="small" defaultValue={4.5} precision={0.5} readOnly />
-                                        <p className="font-light text-gray-800">4.5</p>
-                                    </div>
+        <div>
+            <div className='flex max-md:flex-col max-md:gap-4 items-center justify-between mb-8'>
+                <Breadcrumb />
+                <FilterSort />
+            </div>
+            <div className='grid grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-5'>
+                {
+                    products.length > 0 ?
+                        products.map(product => <div key={product._id} className='border pb-3 rounded-md'>
+                            <Link
+                                href={{
+                                    pathname: `${href + '/' + product._id}`,
+                                    query: {
+                                        title: `${product.name.toLowerCase()}`,
+                                        description: `${product.product_description.toLowerCase()}`
+                                    }
+                                }}
+                            >
+                                <div className="bg-gray-300 bg-opacity-10 rounded w-auto h-52 mb-3">
+                                    <Image src="/images/med1.webp" alt="product-img" width={100} height={100} className="w-full h-full object-cover rounded" />
                                 </div>
-                                <p className='text-sm text-gray-800 mt-2'>{product.product_description.slice(0, 60)}...</p>
+                                <div className='px-3'>
+                                    <h2 className="text-gray-900 hover:underline cursor-pointer mb-2">{product.name}</h2>
+                                    <div className="flex items-center justify-between">
+                                        <h4 className="font-semibold text-gray-800">${product.price}</h4>
+                                        <div className="flex gap-1 items-center">
+                                            <Rating name="half-rating-read" size="small" defaultValue={4.5} precision={0.5} readOnly />
+                                            <p className="font-light text-gray-800">4.5</p>
+                                        </div>
+                                    </div>
+                                    <p className='text-sm text-gray-800 mt-2'>{product.product_description.slice(0, 60)}...</p>
+                                </div>
+                            </Link>
+                            <div className="mt-4 flex gap-2 px-3">
+                                <button onClick={(e) => handleAddToCart(e, product)} className="flex items-center gap-2 justify-center  text-center bg-[#1b1a1af7] text-white  rounded h-10 w-full"><HiShoppingCart /> Add to Cart</button>
+                                <button onClick={(e) => e.stopPropagation()} className="border border-[#161515a5] rounded w-[3.3rem] flex justify-center items-center"><HiOutlineHeart className="text-xl text-[#161515]" /></button>
                             </div>
-                        </Link>
-                        <div className="mt-4 flex gap-2 px-3">
-                            <button onClick={(e) => handleAddToCart(e, product)} className="flex items-center gap-2 justify-center  text-center bg-[#1b1a1af7] text-white  rounded h-10 w-full"><HiShoppingCart /> Add to Cart</button>
-                            <button onClick={(e) => e.stopPropagation()} className="border border-[#161515a5] rounded w-[3.3rem] flex justify-center items-center"><HiOutlineHeart className="text-xl text-[#161515]" /></button>
+                        </div>)
+                        :
+                        <div>
+                            <h3>No products available!</h3>
                         </div>
-                    </div>)
-                    :
-                    <div>
-                        <h3>No products available!</h3>
-                    </div>
-            }
-        </div >
+                }
+            </div >
+        </div>
     );
 };
 
