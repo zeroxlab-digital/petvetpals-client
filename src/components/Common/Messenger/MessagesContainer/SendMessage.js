74f829a7704a8f4ac8ad515cb0168248b1de3dfa
Kazi Solayman <kazisolayman123@gmail.com>
2025-02-25 00:47:35 +0600

    added send messages functionalities with updating messages array

---
+++ src/components/Common/Messenger/MessagesContainer/SendMessage.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { Send } from 'lucide-react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import useFetchMessages from '../../../../../hooks/useFetchMessages';
+
+const SendMessage = ({triggerRefresh}) => {
+    const clickedParticipant = useSelector((state) => state.messageRedu.clickedParticipant);
+    const [message, setMessage] = useState("");
+    const handleSendMessage = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE}/api/message/send/${clickedParticipant._id}`, { message }, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                withCredentials: true,
+            });
+            console.log(res);
+            if(res.status === 201) {
+                triggerRefresh();
+            }
+            setMessage("");
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <form onSubmit={handleSendMessage} className='send-message mt-auto pt-2 relative'>
+            <input type="text" placeholder='Write a message' className='border border-[#58294ea8] w-full p-3 rounded-md outline-[#58294E]'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+            />
+            <button type='submit' className='absolute top-5 right-4 text-gray-800'><Send className='h-5 w-5 text-primary' /></button>
+        </form>
+    );
+};
+
+export default SendMessage;
