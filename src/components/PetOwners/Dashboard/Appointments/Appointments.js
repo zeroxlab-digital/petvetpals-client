31785c560c984c6045c75d8ccaf6678ddc3d0b82
Kazi Solayman <kazisolayman123@gmail.com>
2025-02-27 01:35:05 +0600

    modified appointment related uis

--- src/components/PetOwners/Dashboard/Appointments/Appointments.js
+++ src/components/PetOwners/Dashboard/Appointments/Appointments.js
@@ -58,9 +58,9 @@ const Appointments = () => {
             </ul>
 
             {filtered_appointments.length > 0 ? (
-                <div className='space-y-4'>
+                <div className='space-y-4 max-md:space-y-8'>
                     {filtered_appointments.map((appointment, index) => (
-                        <div key={index} className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-12 items-center gap-5 border rounded-md p-5'>
+                        <div key={index} className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-12 items-center gap-5 md:border md:p-3 max-md:shadow-xl max-md:hover:shadow-2xl duration-200 max-md:bg-white rounded-md'>
                             <div className='text-center lg:col-span-1 max-md:bg-primary max-md:rounded-md max-md:p-3'>
                                 {(() => {
                                     const date = new Date(appointment.date);
@@ -74,7 +74,7 @@ const Appointments = () => {
                                 })()}
                             </div>
 
-                            <div className='flex items-center gap-3 lg:col-span-3'>
+                            <div className='flex items-center gap-3 lg:col-span-3 px-3'>
                                 <Image src={NoPhoto} alt='vet_img' width={200} height={200} className='w-14 h-auto rounded-full' />
                                 <div>
                                     <h5 className='font-semibold text-lg mb-1'>{appointment.vet?.fullName}</h5>
@@ -82,7 +82,7 @@ const Appointments = () => {
                                 </div>
                             </div>
 
-                            <div className='lg:col-span-3'>
+                            <div className='lg:col-span-3 px-3'>
                                 <p className='text-gray-600 flex items-center gap-2 mb-2'>
                                     <HiOutlineClock className='text-lg' />
                                     {(() => {
@@ -94,23 +94,23 @@ const Appointments = () => {
                                 <p className='text-gray-600 flex items-center gap-2'><HiOutlineMapPin className='text-lg' /> Online</p>
                             </div>
 
-                            <div className='lg:col-span-2'>
-                                <p className='text-gray-600 flex items-center gap-2'><LuDog className='text-lg' /> {appointment?.pet?.name || 'N/A'} ({appointment?.pet?.type || 'N/A'})</p>
+                            <div className='lg:col-span-2 px-3'>
+                                <p className='text-gray-600 flex items-center gap-2 mb-2'><LuDog className='text-lg' /> {appointment?.pet?.name || 'Unavailable'} ({appointment?.pet?.type || ''})</p>
                                 <p className='text-gray-600 flex items-center gap-2'><HiOutlineCurrencyDollar className='text-lg' /> {appointment.payment_status ? 'Paid' : 'Unpaid'}</p>
                             </div>
 
-                            <div className='flex flex-wrap gap-4 lg:col-span-3 justify-end'>
+                            <div className='flex flex-wrap gap-4 lg:col-span-3 justify-end p-3'>
                                 {active_status_tab === 'confirmed' ? (
-                                    <>
-                                        <button className='bg-white px-5 py-3 rounded-lg border text-gray-900 text-sm flex items-center gap-2 max-lg:w-full max-lg:justify-center'>Edit <HiChevronDown /></button>
-                                        <button className='bg-primary px-5 py-3 rounded-lg text-white text-sm flex items-center gap-2 max-lg:w-full max-lg:justify-center'>Join Now <HiVideoCamera /></button>
-                                    </>
+                                    <div className='w-full flex items-center max-md:flex-col gap-3'>
+                                        <button className='bg-white px-5 py-3 rounded-lg border text-gray-900 text-sm flex items-center gap-2 justify-center md:max-w-max max-md:w-full'>Edit <HiChevronDown /></button>
+                                        <button className='bg-primary px-5 py-3 rounded-lg text-white text-sm flex items-center gap-2  justify-center min-w-max max-md:w-full'>Join Now <HiVideoCamera /></button>
+                                    </div>
                                 ) : active_status_tab === 'pending' ? (
-                                    <>
-                                        <button onClick={() => handleConfirmBooking(appointment)} className='bg-primary px-5 py-3 rounded-lg text-white text-sm flex items-center gap-2 max-lg:w-4/5 max-lg:justify-center'>Confirm <HiArrowRight /></button>
-                                        <button onClick={() => handleAppointmentDlt(appointment._id)} className='bg-white px-3 py-3 rounded-lg text-red-400 border text-sm flex items-center  '><HiOutlineTrash /></button>
+                                    <div className='w-full flex gap-3 '>
+                                        <button onClick={() => handleConfirmBooking(appointment)} className='bg-primary px-5 py-3 rounded-lg text-white text-sm flex items-center gap-2  justify-center w-full'>Confirm <HiArrowRight /></button>
+                                        <button onClick={() => handleAppointmentDlt(appointment._id)} className='bg-white p-3 rounded-lg text-primary border border-[#58294E] flex items-center  '><HiOutlineTrash className='h-5 w-5' /></button>
                                         {showModal && <ConfirmBookingModal apptId={clickedAppointment._id} setShowModal={setShowModal} />}
-                                    </>
+                                    </div>
                                 ) : (
                                     <button className='bg-primary px-5 py-3 rounded-lg text-white text-sm flex items-center gap-2'>Give Feedback <HiOutlineStar /></button>
                                 )}
--- src/components/PetOwners/Dashboard/Appointments/ConfirmBookingModal.js
+++ src/components/PetOwners/Dashboard/Appointments/ConfirmBookingModal.js
@@ -13,22 +13,22 @@ const ConfirmBookingModal = ({ setShowModal, apptId }) => {
     }, [])
     return (
         <div className="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center z-50">
-            <div className="relative w-[90%] max-w-[50rem] bg-white shadow-lg rounded-lg overflow-hidden hide-scrollbar">
+            <div className="relative w-[95%] max-w-[50rem] bg-white shadow-lg rounded-lg overflow-hidden hide-scrollbar">
 
-                <div className='overflow-auto hide-scrollbar h-[80%] lg:h-[30rem] p-4'>
+                <div className='overflow-auto hide-scrollbar h-[80%] lg:h-max p-4'>
                     {
-                    bookingStage == "pet-details" ?
-                    <BookingDetails apptId={apptId} setBookingState={setBookingState} />
-                    :
-                    bookingStage == "payment-details" ?
-                    <BookingPayment apptId={apptId} setShowModal={setShowModal} />
-                    :
-                    null
+                        bookingStage == "pet-details" ?
+                            <BookingDetails apptId={apptId} setBookingState={setBookingState} />
+                            :
+                            bookingStage == "payment-details" ?
+                                <BookingPayment apptId={apptId} setShowModal={setShowModal} />
+                                :
+                                null
                     }
                 </div>
 
                 <button
-                    className="absolute sm:top-2 right-2 max-sm:right-0 w-8 h-8 flex items-center justify-center text-gray-500 hover:text-gray-700"
+                    className="absolute top-2 right-2 sm:right-2 sm:top-2 max-sm:right-2 max-sm:top-2 w-8 h-8 flex items-center justify-center text-gray-500 hover:text-red-700"
                     onClick={() => setShowModal(false)}
                 >
                     <HiXMark size={25} />
