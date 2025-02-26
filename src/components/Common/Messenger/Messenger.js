386afc86e38c43dd47e4ed717d378480d49a7f0e
Kazi Solayman <kazisolayman123@gmail.com>
2025-02-27 02:45:16 +0600

    moddified messenger ui for mobile version

--- src/components/Common/Messenger/Messenger.js
+++ src/components/Common/Messenger/Messenger.js
@@ -8,12 +8,13 @@ import Image from 'next/image';
 const Messenger = () => {
     const authUser = useSelector((state) => state.userRedu.user.authUser);
     const clickedParticipant = useSelector((state) => state.messageRedu.clickedParticipant);
+    console.log("Clicked Participant:", clickedParticipant);
     return (
-        <div className='grid grid-cols-[5fr_2fr] max-md:grid-cols-1 gap-5 '>
+        <div className='md:grid md:grid-cols-[5fr_2fr] max-md:grid-cols-1 gap-5 h-full'>
             {clickedParticipant ?
                 <ConversationContainer clickedParticipant={clickedParticipant} authUser={authUser} />
                 :
-                <div className="flex flex-col items-center justify-center text-center max-md:order-2">
+                <div className="flex flex-col items-center justify-center text-center max-md:order-2 max-md:hidden">
                     <Image src="/images/pet-care.jpg" alt="vet" width={100} height={100} className='w-72' />
                     <h1 className='font-bold text-2xl text-primary mb-3 mt-5'>Choose a vet you want to chat with!</h1>
                     <p className='text-gray-600'>Select a vet available and ask anything regarding your furry family member</p>
