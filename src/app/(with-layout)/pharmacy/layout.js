75825c390ae78080d16b507512ce94fc475ee931
Kazi Solayman <kazisolayman123@gmail.com>
2025-03-06 02:20:09 +0600

    added custom metadata for different pages

--- src/app/(with-layout)/pharmacy/layout.js
+++ src/app/(with-layout)/pharmacy/layout.js
@@ -1,6 +1,11 @@
 import CategoriesSidebar from '@/e-commerce/CategoriesSidebar/CategoriesSidebar';
 import React from 'react';
 
+export const metadata = {
+    title: "Pet Pharmacy - Medications & Supplements | PetVetPals",
+    description: "Shop vet-approved pet medications, supplements, and health products at PetVetPals. Keep your pets healthy with trusted pharmacy essentials!",
+};
+
 const PharmacyLayout = ({ children }) => {
     return (
         <div className='app-container py-10 grid lg:grid-cols-5 gap-10'>
--- src/app/(with-layout)/shop/layout.js
+++ src/app/(with-layout)/shop/layout.js
@@ -1,6 +1,11 @@
 import CategoriesSidebar from '@/e-commerce/CategoriesSidebar/CategoriesSidebar';
 import React from 'react';
 
+export const metadata = {
+    title: "Shop Pet Foods & Accessories | PetVetPals",
+    description: "Browse high-quality pet foods, toys, accessories, and more at PetVetPals. Find the best products to keep your pets happy and healthy!",
+};
+
 const ShopLayout = ({ children }) => {
     return (
         <div className='app-container py-10 grid lg:grid-cols-5 gap-10'>
