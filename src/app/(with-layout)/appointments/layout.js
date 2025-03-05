52137ac545d8b7575fba34fac00b98f161f8cddd
Kazi Solayman <kazisolayman123@gmail.com>
2025-03-06 02:20:09 +0600

    added custom metadata for different pages

--- src/app/(with-layout)/appointments/layout.js
+++ src/app/(with-layout)/appointments/layout.js
@@ -1,4 +1,10 @@
 
+export const metadata = {
+    title: "Book a Vet Appointment | Find Trusted Veterinarians - PetVetPals",
+    description: "Easily find and book appointments with experienced veterinarians near you. Get the best care for your pets with PetVetPals' trusted vet network.",
+};
+
+
 const AppointmentsLayout = ({ children }) => {
     return (
         <div>
