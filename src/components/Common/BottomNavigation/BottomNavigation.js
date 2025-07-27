f240a493a03699ff8c33d9a821f26653980191ae
Kazi Solayman <kazisolayman123@gmail.com>
2025-07-27 13:09:09 -0500

    Modified Bottom navigation

--- src/components/Common/BottomNavigation/BottomNavigation.js
+++ src/components/Common/BottomNavigation/BottomNavigation.js
@@ -8,47 +8,82 @@ import {
   FaHouse,
   FaPaw,
   FaPills,
+  FaStethoscope,
   FaUser
 } from 'react-icons/fa6';
+import { LucideStethoscope, Stethoscope, StethoscopeIcon } from 'lucide-react';
+import { HiOutlineCalendar, HiOutlineHome, HiOutlineUser, HiOutlineUserCircle, HiUser } from 'react-icons/hi2';
+import { LuStethoscope } from 'react-icons/lu';
+import Image from 'next/image';
 
 const BottomNavigation = () => {
   const pathname = usePathname();
 
   const links = [
-    { title: "My Pets", link: "/dashboard/pets", icon: <FaPaw /> },
-    { title: "Vet GPT", link: "/dashboard/symptom-checker", icon: <FaPills /> },
-    { title: "Dashboard", link: "/dashboard", icon: <FaHouse /> },
-    { title: "Appointments", link: "/dashboard/appointments", icon: <FaCalendar /> },
-    { title: "Pet Owner", link: "/dashboard/account", icon: <FaUser /> }
+    {
+      title: "My Pets",
+      link: "/dashboard/pets",
+      // image: "/images/paw.svg"
+    },
+    {
+      title: "Vet GPT",
+      link: "/dashboard/symptom-checker",
+      icon: <LuStethoscope />
+    },
+    {
+      title: "Dashboard",
+      link: "/dashboard",
+      icon: <HiOutlineHome />
+    },
+    {
+      title: "Appointments",
+      link: "/dashboard/appointments",
+      icon: <HiOutlineCalendar />
+    },
+    {
+      title: "Pet Owner",
+      link: "/dashboard/account",
+      icon: <HiOutlineUserCircle />
+    }
   ];
+
 
   return (
     <motion.div
       initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ type: 'spring', stiffness: 200, damping: 25 }}
-      className='lg:hidden bg-white fixed bottom-0 left-0 w-full px-2 py-3 z-20 border-t border-gray-200 rounded-t-2xl shadow-2xl shadow-black'
+      className='lg:hidden bg-white fixed bottom-0 left-0 w-full z-20 border-t border-gray-200 rounded-t-2xl shadow-2xl shadow-black'
     >
-      <ul className='grid grid-cols-5 items-center justify-between text-center'>
+      <ul className='h-14 grid grid-cols-5 items-center justify-between text-center'>
         {links.map((link, index) => {
           const isActive = pathname === link.link;
-
           return (
-            <li key={index} className='text-xl cursor-pointer'>
-              <Link href={link.link} className='flex flex-col gap-[6px] items-center'>
+            <Link href={link.link} key={index} className="text-xl cursor-pointer ">
+              <li className="flex flex-col gap-1 items-center">
                 <motion.span
                   whileTap={{ scale: 0.95 }}
                   animate={{ scale: isActive ? 1.15 : 1 }}
                   transition={{
-                    type: 'spring',
+                    type: "spring",
                     stiffness: 200,
                     damping: 20,
-                    mass: 0.5
+                    mass: 0.5,
                   }}
-                  className={isActive ? 'text-primary' : 'text-gray-600'}
+                  className={`text-xl ${isActive ? "text-primary" : "text-gray-600"}`}
                 >
-                  {link.icon}
+                  {link.icon ? (
+                    link.icon
+                  ) : (
+                    <Image
+                      src={`${isActive ? '/images/paw-primary.svg' : '/images/paw-gray.svg'}`}
+                      alt={'paw'}
+                      width={25}
+                      height={25}
+                    />
+                  )}
                 </motion.span>
+
                 <AnimatePresence>
                   {isActive && (
                     <motion.span
@@ -56,15 +91,15 @@ const BottomNavigation = () => {
                       initial={{ opacity: 0, y: 5 }}
                       animate={{ opacity: 1, y: 0 }}
                       exit={{ opacity: 0, y: 5 }}
-                      transition={{ duration: 0.25, ease: 'easeInOut' }}
+                      transition={{ duration: 0.25, ease: "easeInOut" }}
                       className="text-sm text-primary"
                     >
                       {link.title}
                     </motion.span>
                   )}
                 </AnimatePresence>
-              </Link>
-            </li>
+              </li>
+            </Link>
           );
         })}
       </ul>
