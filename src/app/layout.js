2c710b6a4b4aa31fed53bcb98869f5d7134ae820
Kazi Solayman <kazisolayman123@gmail.com>
2025-09-01 02:33:38 -0500

    Included sitemap, manifest and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,17 @@ import "./globals.css";
 import StoreProvider from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
-const baloo2 = Baloo_2({ subsets: ["latin"], weight: ["400","600","700"] });
+const baloo2 = Baloo_2({ subsets: ["latin"], weight: ["400", "600", "700"] });
 
 export const metadata = {
   title: "PetVetPals – Your Trusted Online Pet Care Platform",
   description: "AI-Powered pet health tools, verified veterinarians, online vet consultations, shop pet accessories, and ensure your pet's well-being—all in one place!",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/icons/icon-192x192.png"
+  }
 };
 
 const RootLayout = ({ children }) => {
