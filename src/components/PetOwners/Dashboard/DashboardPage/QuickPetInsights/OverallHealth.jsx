bf115ca9cc69f658f8669fcc5e6f375904e7c1e9
Kazi Solayman <kazisolayman123@gmail.com>
2025-11-18 17:29:22 -0600

    Added health status based on health score

--- src/components/PetOwners/Dashboard/DashboardPage/QuickPetInsights/OverallHealth.jsx
+++ src/components/PetOwners/Dashboard/DashboardPage/QuickPetInsights/OverallHealth.jsx
@@ -2,6 +2,22 @@ import { Activity } from 'lucide-react';
 import React from 'react';
 
 const OverallHealth = ({ petData }) => {
+    const healthStatus = () => {
+        const health_score = petData?.overall_health;
+        switch (true) {
+            case health_score < 30:
+                return "Unhealthy"
+            case health_score < 50:
+                return "Moderate"
+            case health_score < 80:
+                return "Healthy"
+            case health_score < 100:
+                return "Excellent"
+            default:
+                return "Unknown"
+        }
+    }
+    console.log(healthStatus())
     return (
         <div className="bg-white rounded-xl border p-4 shadow-sm hover:shadow-md transition-shadow">
             <div className="flex items-center justify-between pb-2">
@@ -9,7 +25,7 @@ const OverallHealth = ({ petData }) => {
                 <Activity className="h-4 w-4 text-green-500" />
             </div>
             <div className="text-2xl font-bold text-green-500">
-                Excellent
+                {healthStatus()}
             </div>
             <div className="mt-2 h-2 rounded-full bg-gray-100 overflow-hidden">
                 <div
